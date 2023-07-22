f1e1d9ea9383eb393938b37f0f14c5387797d14c
T448 <tatsuda4480712@gmail.com>
2023-07-22 03:47:43 +0000

    🩹画面移動時のセッション確認方法の修正

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import outerFrame from './components/outerFrame.vue';
 import top from './components/top.vue';
+import calendarView from './components/calendarView.vue';
 import axios from 'axios';
 
 const routes = [
-    { path: '/app/main', name: 'main', component: outerFrame },
     { path: '/app', name: 'top', component: top },
+    { path: '/app/main', name: 'main', component: outerFrame },
+    { path: '/app/calendar', name: 'calendar', component: calendarView },
 ]
 
 const router = createRouter({
@@ -17,7 +19,11 @@ router.beforeEach((to, from, next) => {
     if ((from.name == "top" && to.name == "main") || to.name == "top") {
         // login時にセッションを発行するのでそのまま通す。
         next();
-    } else {
+    } else if (to.matched.length == 0) {
+        // 行先が定義されていなければmainに戻す
+        router.push({ name: "main" });
+    }
+    else {
         axios
             .post("http://localhost:8080/api/session", {}, { withCredentials: true })
             .then(res => {
