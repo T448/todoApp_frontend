ee933ef0fd45d3d8f21438d042401cf991df915c
T448 <tatsuda4480712@gmail.com>
2023-09-07 13:37:19 +0000

    全体的にリファクタリングした

--- src/components/beforeLogin.vue
+++ src/components/beforeLogin.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { inject } from "vue";
-import type { CallbackTypes } from "vue3-google-login";
-import { decodeCredential } from "vue3-google-login";
+import { googleAuthCodeLogin } from "vue3-google-login"
 import { useRouter } from 'vue-router';
 import { VueCookies } from "vue-cookies";
 import axios from "axios";
@@ -9,21 +8,24 @@ import axios from "axios";
 const router = useRouter()
 const $cookies = inject<VueCookies>('$cookies');
 
-const callback: CallbackTypes.CredentialCallback = (response) => {
-    const res: any = decodeCredential(response.credential);
-    axios
-        .post("http://localhost:8080/api/login", { email: res.email, name: res.name }, { withCredentials: true })
-        .then(res => {
-            if (res.data.sessionID) {
-                $cookies?.set("sessionID", res.data.sessionID)
-                console.log("login");
-                router.push({ name: 'main' });
-            } else {
-                alert("不正なログイン1");
-            }
+
+const login = () => {
+    googleAuthCodeLogin()
+        .then((response) => {
+            axios
+                .post('http://localhost:8080/api/login', {},
+                    { headers: { "Authorization": response.code, "Content-Type": "application/json" }, withCredentials: true })
+                .then((response_from_oauth2) => {
+                    if (response_from_oauth2.status == 200) {
+                        $cookies?.set("sessionID", response_from_oauth2.data.sessionId)
+                        router.push({ name: 'main' });
+                    } else {
+                        alert("error");
+                    }
+                })
         })
-        .catch(() => { alert('不正なログイン2') });;
-};
+
+}
 </script>
 
 <template>
@@ -31,7 +33,8 @@ const callback: CallbackTypes.CredentialCallback = (response) => {
         <h1 style="color: white;padding: 0px;margin: 0px;box-shadow: 5px;">
             todoApp
         </h1>
-        <GoogleLogin :callback="callback" prompt auto-login />
+        <!-- <GoogleLogin :callback="callback" prompt auto-login /> -->
+        <button @click="login">Login Using Google</button>
     </div>
     <div id="beforeLogin" style="vertical-align: center;">
         <img src="/src/assets/undraw_world_re_768g.svg"
@@ -47,11 +50,6 @@ const callback: CallbackTypes.CredentialCallback = (response) => {
     display: flex;
     justify-content: space-between;
     height: 50px;
-    /* display: flex;
-    left: 0;
-    top: 0;
-    margin: 0;
-    */
     z-index: 1001;
     box-shadow: 0 2px 3px rgba(255, 255, 255, 0.25);
 }
