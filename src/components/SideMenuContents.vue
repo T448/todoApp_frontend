1b563e9602751c5af6ff60a839457d39c2202b15
T448 <tatsuda4480712@gmail.com>
2023-09-09 22:24:15 +0900

    SideMenuContentsの見た目の変更

--- src/components/SideMenuContents.vue
+++ src/components/SideMenuContents.vue
@@ -3,21 +3,31 @@
 </script>
 
 <template>
-    <table style="color: aliceblue;">
-        <tr>
-            <td>
-                hoge1
-            </td>
-        </tr>
-        <tr>
-            <td>
-                hoge2
-            </td>
-        </tr>
-        <tr>
-            <td>
-                hoge3
+    <table>
+        <tr v-for="n of 5">
+            <td class="project-item">
+                hoge+{{ n }}
             </td>
         </tr>
     </table>
 </template>
+
+<style scoped>
+table {
+    color: white;
+    width: 100%
+}
+
+tr {
+    width: 100%;
+}
+
+.project-item {
+    padding: 8px;
+    width: 100%;
+}
+
+.project-item:hover {
+    background-color: #3c3c3c;
+}
+</style>
