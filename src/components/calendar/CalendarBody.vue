b199742824b7f1559fb0d11095a49edaf4b9bb1c
T448 <tatsuda4480712@gmail.com>
2023-09-23 00:25:20 +0900

    ✨イベント追加用ダイアログを追加した(UIのみ)

--- src/components/calendar/CalendarBody.vue
+++ src/components/calendar/CalendarBody.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 import OneDay from './OneDay.vue';
+import AddEventDialog from '../AddEventDialog.vue';
+import { ref } from 'vue';
+
+const isShowDialog = ref(false);
+
+const showAddEventDialog = () => {
+    isShowDialog.value = true;
+};
+const closeAddEventDialog = () => {
+    isShowDialog.value = false;
+}
+const stopEvent = () => {
+    event?.stopPropagation();
+}
 </script>
 
 <template>
     <div>
         <img src="../../assets/icons8-back-30.png" class="month-button">
-        <span style="color:white;font-size:28px">9月</span>
+        <span style="color:white;font-size:28px">2023年9月</span>
         <img src="../../assets/icons8-forward-30.png" class="month-button">
+        <button @click="showAddEventDialog">+</button>
     </div>
     <table class="calendarTable">
         <thead style="color:white">
@@ -21,11 +36,16 @@ import OneDay from './OneDay.vue';
         <tbody>
             <tr v-for="week of 5">
                 <td v-for="day of 7">
-                    <OneDay :date="day" />
+                    <OneDay :date="Number(String(week) + String(day))" @show-add-event-dialog="showAddEventDialog" />
                 </td>
             </tr>
         </tbody>
     </table>
+    <div v-if="isShowDialog" @click.stop="closeAddEventDialog" id="overlay">
+        <div id="content" @click="stopEvent">
+            <AddEventDialog @close-dialog="closeAddEventDialog" />
+        </div>
+    </div>
 </template>
 
 <style scoped>
@@ -34,6 +54,7 @@ import OneDay from './OneDay.vue';
     width: 100%;
     height: 95%;
     border-spacing: 0;
+    margin-bottom: 10px;
 }
 
 .month-button {
@@ -47,5 +68,31 @@ import OneDay from './OneDay.vue';
 
 .month-button:hover {
     opacity: 1;
+}
+
+#overlay {
+    /*　要素を重ねた時の順番　*/
+    z-index: 1;
+
+    /*　画面全体を覆う設定　*/
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+
+    /*　画面の中央に要素を表示させる設定　*/
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+}
+
+#content {
+    z-index: 2;
+    max-width: 50%;
+    max-height: 50%;
+    background: #fff;
 }
 </style>
--- src/components/calendar/OneDay.vue
+++ src/components/calendar/OneDay.vue
@@ -5,22 +5,44 @@ const props = defineProps({
     date: Number,
     tasks: Array<string>,
 })
-
+const emits = defineEmits<{
+    (e: 'showAddEventDialog'): void
+}>();
 const date = ref(props.date);
 const tasks = ref(props.tasks);
 
+const showAddButton = ref(false);
+const mouseOverAction = () => {
+    showAddButton.value = true;
+}
+const mouseLeaveAction = () => {
+    showAddButton.value = false;
+}
+
+
+const onClickAddButton = () => {
+    console.log('OneDayのほう');
+    emits('showAddEventDialog');
+}
 // date.value = new Date();
 tasks.value = ['hoge', 'fuga'];
 </script>
 
 <template>
-    <div class="one-day">
+    <div v-on:mouseover="mouseOverAction" v-on:mouseleave="mouseLeaveAction" class="one-day">
         <div class="date">
             {{ date }}
         </div>
         <div v-for="(task, index) of tasks" class="task">
-            <input type="checkbox">
+            <input type="checkbox" :key="index">
             <label>{{ task }}</label>
+        </div>
+        <div style="text-align: center;">
+            <span v-if="showAddButton" @click="onClickAddButton"
+                style="display: inline-block; width: 20x; height: 20px; border-radius: 50%; background: skyblue; text-align:center; line-height: 20px;">
+                +
+            </span>
+            <span v-else>-</span>
         </div>
     </div>
 </template>
